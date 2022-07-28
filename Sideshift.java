24da13dee6d39d28375d0b3cf82fe093a6dc5b84
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-07-28 08:04:50 -0300

    Create Sideshift.java

---
+++ Sideshift.java
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+  window.__SIDESHIFT__ = {
+    parentAffiliateId: "fkEyFD3QkS",
+    defaultDepositMethodId: "btc",
+    defaultSettleMethodId: "eth",
+    settleAddress: undefined,
+  }
+</script>
+<script src="https://sideshift.ai/static/js/main.js"></script>
